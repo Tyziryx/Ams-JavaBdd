34bfafdef10be9a265c31d5c1a8c18097191b923
Tyziryx <alexim13550@gmail.com>
2024-11-30 22:12:30 +0100

    Creation Connexion + mis qq trucs dedans

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.sql.*;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.Executor;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+
+
+    Connexion cn = new Connexion() ;
+
+
+
 }
