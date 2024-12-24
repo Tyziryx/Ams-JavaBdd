926cae17ff8c0c0924d0f68a1796a97ea8a034b5
Tidic <tidic84@gmail.com>
2024-12-24 05:54:41 +0100

    Reorganisation du css et garder l'onglet actif

--- src/main/java/ui/components/Navbar.java
+++ src/main/java/ui/components/Navbar.java
@@ -10,12 +10,15 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import main.java.ui.pages.*;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Navbar extends VBox {
 
@@ -24,25 +27,31 @@ public class Navbar extends VBox {
     public Navbar(BorderPane page) throws SQLException, IOException {
         this.page = page;
         this.setSpacing(10);
+        VBox vBox = new VBox();
+        Text navbarTitle = new Text("Masup");
+        navbarTitle.getStyleClass().add("navbar-title");
+        vBox.getChildren().add(navbarTitle);
+        vBox.getStyleClass().add("navbar");
+        vBox.prefHeightProperty().bind(page.heightProperty());
 
-        Label navbarTitle = new Label("");
-        Map<String, Page> buttonList = new LinkedHashMap<String, Page>() {{
+        Map<String, Page> addButtonList = new LinkedHashMap<String, Page>() {{
             put("Accueil", new PagePrincipale(20));
             put("Fournisseurs", new PageFournisseurs(20));
             put("Achats", new PageAchats(20));
             put("Ventes", new PageVentes(20));
             put("Stock", new PageStock(20));
         }};
+        HashSet<Button> buttonList = new HashSet<>();
 
-        for (Map.Entry<String, Page> entry : buttonList.entrySet()) {
+        for (Map.Entry<String, Page> entry : addButtonList.entrySet()) {
             // Récup du nom et de la page
             String name = entry.getKey();
             Page pageType = entry.getValue();
-            this.getStyleClass().add("navbar");
 
             // Creation et setup du boutton
             Button button = new Button();
             button.getStyleClass().add("navbar-button"); // Classe générale du bouton
+            if (name.equals("Accueil")) { button.getStyleClass().add("active"); }
             HBox hBox = new HBox(10); // Espacement entre icône et texte
 
             // Création de l'icône SVG via la classe CSS
@@ -80,11 +89,18 @@ public class Navbar extends VBox {
                 public void handle(ActionEvent event) {
                     pageType.getStyleClass().add("content");
                     page.setCenter((Node) pageType);
+                    for (Button b : buttonList) {
+                        b.getStyleClass().remove("active");
+                    }
+                    button.getStyleClass().add("active");
                 }
             });
 
             // Ajout du bouton a la navbar
-            this.getChildren().add(button);
+            buttonList.add(button);
+            vBox.getChildren().add(button);
         }
+
+        this.getChildren().add(vBox);
     }
 }
